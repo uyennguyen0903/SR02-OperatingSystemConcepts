c95a9fe42ebf4f13558bebd2cea69e2c557b8d58
NGUYENUYEN0903 <44533077+NGUYENUYEN0903@users.noreply.github.com>
2020-03-23 17:22:38 +0100

    Update EX1.c

--- TD2/EX1.c
+++ TD2/EX1.c
@@ -8,12 +8,32 @@ struct sigaction act_thermometre, act_alarme;
 pid_t pid;
 
 void handler_thermometre(int x){
-
+	int rnd=(random()/(RAND_MAX*1.0))*40;
+	printf("Temperature = %d C°\n",rnd);
 }
 
 void handler_alarme(int x){
-
+	kill(pid,SIGUSR1);
+	alarm(5);
 }
 main(){
-
+	pid=fork();
+	if(pid==0){
+		/*processus fils: thermometre*/
+		act_thermometre.sa_handler=handler_thermometre;
+		sigaction(SIGUSR1,&act_thermometre,NULL);
+		while(1){
+			pause();
+		}
+	}else{
+		/*processus pere: mesure périodique*/
+		act_alarme.sa_handler=handler_alarme;
+		sigaction(SIGALRM,&act_alarme,NULL);
+		alarm(5);
+		while(1){
+			sleep(1);
+			printf("-");
+			fflush(stdout);
+		}
+	}
 }
